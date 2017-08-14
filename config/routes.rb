f70cc87c25769b2e0abc3b09417c313a8c62265d
Karena Stevens <kaymark09@gmail.com>
2017-08-14 16:48:18 -0400

    Readded Static Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   
+  get 'static_pages/home'
+
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
+
+  get 'static_pages/businesses'
+
+  root 'static_pages#home'
   get 'home/about'
-
   get 'home/contact'
-
   get 'home/businesses'
-
-  root 'application#hello'
 end
